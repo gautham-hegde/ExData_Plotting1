c9d8cfa03210e0a9b0a68d73373d00f6068105d0
Hegde, Gautham (SFO-MBW) <Gautham.Hegde@mbww.com>
2014-11-09 22:42:24 -0800

    corrected code

--- plot2.R
+++ plot2.R
@@ -2,12 +2,14 @@
 electricPower <- read.csv('data/household_power_consumption.txt', sep=';', na.strings='?')
 
 electricPower$Date <- as.Date(electricPower$Date, format='%d/%m/%Y')
-##electricPower$Time <- strptime(electricPower$Time,)
 
 #get only the 2007 set
 electricPower <- electricPower[electricPower$Date >= '2007-02-01' & electricPower$Date <= '2007-02-02',]
 
-plot(electricPower$Global_active_power, type = 'l')
+electricPower$DateTime <- paste(electricPower$Date, electricPower$Time)
+electricPower$DateTime <- strptime(electricPower$DateTime, format = "%Y-%m-%d %H:%M:%S")
+
+plot(electricPower$DateTime, electricPower$Global_active_power, type = 'l', ylab = 'Global Active Power (kilowatts)')
 
 dev.copy(png, file='figure/plot2.png')
 
